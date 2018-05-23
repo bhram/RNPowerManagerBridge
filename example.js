2ac871b482b12ca8b4233cb60d4b246caa4e1022
bhram12 <bhram12@gmail.com>
2018-05-23 08:02:46 +0300

    init project

---
+++ example.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PowerManager from './PowerManagerBridge';
+
+export default class App extends Component {
+  async componentDidMount() {
+   const a= await PowerManager.isHasProtectManager();
+   if(a){
+    PowerManager.startIntent(a.package,a.class);
+   }
+    alert(JSON.stringify(a));
+    PowerManager.show('LONG', PowerManager.LONG)
+  }
+ 
+}
